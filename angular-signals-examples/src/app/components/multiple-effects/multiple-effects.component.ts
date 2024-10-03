c45eb111d52ae540f67a272cd59eeac79a25f46a
Stefan Patrascu <stefan.patrascu@mindit.io>
2024-10-03 17:28:22 +0300

    added clean-up for effects

--- angular-signals-examples/src/app/components/multiple-effects/multiple-effects.component.ts
+++ angular-signals-examples/src/app/components/multiple-effects/multiple-effects.component.ts
@@ -1,4 +1,4 @@
-import {Component, effect, signal, WritableSignal} from '@angular/core';
+import {Component, effect, EffectRef, OnDestroy, signal, WritableSignal} from '@angular/core';
 
 @Component({
   selector: 'app-counter-with-multiple-effects',
@@ -10,14 +10,16 @@ import {Component, effect, signal, WritableSignal} from '@angular/core';
   <button (click)="increaseCounter()">Increase Counter</button>`
 
 })
-export class MultipleEffectsComponent {
+export class MultipleEffectsComponent implements OnDestroy {
 
   score: WritableSignal<number> = signal(0);
   counter: WritableSignal<number> = signal(0);
   anotherCounter: WritableSignal<number> = signal(0);
+  effectRef!: EffectRef;
+  effectRef2!: EffectRef;
 
   constructor() {
-    effect(() => {
+    this.effectRef = effect(() => {
       // this method will be triggered ONLY when counter or anotherCounter has been updated
       if (this.anotherCounter() === 0) {
         console.log(`COUNTER: ${this.counter()}`);
@@ -26,7 +28,7 @@ export class MultipleEffectsComponent {
       }
     });
 
-    effect(() => {
+    this.effectRef2 = effect(() => {
       // this method will be triggered ONLY when score has been updated
       console.log(`SCORE: ${this.score()}`);
     });
@@ -38,6 +40,11 @@ export class MultipleEffectsComponent {
 
   increaseCounter() {
     this.counter.update(value => value + 1); // this will increment the current value of counter by 1
+  }
+
+  ngOnDestroy() {
+    this.effectRef.destroy();
+    this.effectRef2.destroy();
   }
 
 }
