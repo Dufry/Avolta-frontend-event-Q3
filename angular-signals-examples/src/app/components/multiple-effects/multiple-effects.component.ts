46f76e08f8717c4072f3131cfabd2e0a1a64e50e
Stefan Patrascu <stefan.patrascu@mindit.io>
2024-10-03 16:22:13 +0300

    Multiple effect() methods on the same component

---
+++ angular-signals-examples/src/app/components/multiple-effects/multiple-effects.component.ts
@@ -0,0 +1,41 @@
+import {Component, effect, signal, WritableSignal} from '@angular/core';
+
+@Component({
+  selector: 'app-counter-with-multiple-effects',
+  standalone: true,
+  template: `
+    The current value of score is: {{ score() }} <br/>
+  The current value of counter is: {{ counter() }} <br/>
+  <button (click)="increaseScore()">Increase Score</button>
+  <button (click)="increaseCounter()">Increase Counter</button>`
+
+})
+export class MultipleEffectsComponent {
+
+  score: WritableSignal<number> = signal(0);
+  counter: WritableSignal<number> = signal(0);
+  anotherCounter: WritableSignal<number> = signal(0);
+
+  constructor() {
+    effect(() => {
+      // this method will be triggered ONLY when score and anotherCounter are updated
+      if (this.anotherCounter() === 0) {
+        console.log(`SCORE: ${this.score()}`);
+      }
+    });
+
+    effect(() => {
+      // this method will be triggered ONLY when counter is updated
+      console.log(`COUNTER: ${this.counter()}`);
+    });
+  }
+
+  increaseScore() {
+    this.score.update(value => value + 1); // this will increment the current value of score by 1
+  }
+
+  increaseCounter() {
+    this.counter.update(value => value + 1); // this will increment the current value of score by 1
+  }
+
+}
